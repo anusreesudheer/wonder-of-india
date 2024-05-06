65571396eedf882f7a62fa2ffe8b61b54326670c
anusreesudheer <153237299+anusreesudheer@users.noreply.github.com>
2024-05-06 15:23:31 +0300

    updated compl

--- src/Shared/Tourcard.jsx
+++ src/Shared/Tourcard.jsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom'
 
 function Tourcard({tour}) {
 const { _id,title, city, photo , price, avgrating,}=tour;
-
+console.log("Photo prop:", photo);
   return (
     <div className="tour_card">
     <Card>
